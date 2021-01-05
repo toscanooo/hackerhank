9c63184713f6d5e1198be6256eab78d2ebb17b77
Marcelo Toscano <75312780+toscanooo@users.noreply.github.com>
2021-01-04 21:43:57 -0300

    Create day1DataTypes.swift
    Solução para day 1 data types https://www.hackerrank.com/challenges/30-data-types/problem

---
+++ day1DataTypes.swift
@@ -0,0 +1,13 @@
+var i = 4
+var d = 4.0
+var s = "HackerRank "
+var _i: Int
+
+guard let _i:Int = Int(readLine()!) else { fatalError("Bad input") }
+guard let _d:Double = Double(readLine()!) else { fatalError("Bad input") }
+guard let _s:String = String(readLine()!) else { fatalError("Bad input") }
+
+
+print(i + _i)
+print(d + _d)
+print(s + _s)
